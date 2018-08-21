c08eee35ab25bd45329d267fb437ab70f5958b46
RJMM <r.j.m.meneses@gmail.com>
2018-08-21 04:13:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 var animation = bodymovin.loadAnimation({
-<<<<<<< HEAD
   container: document.getElementById('bm'),
   renderer: 'svg',
   loop: true,
   autoplay: true,
   path: 'data.json'
 })
-=======
- container: document.getElementById('bm'),
- renderer: 'svg',
- loop: true,
- autoplay: true,
- path:'data.json'
-})
->>>>>>> c250c8d760a0f819bfb9e2908b720b51e30bc100
+
